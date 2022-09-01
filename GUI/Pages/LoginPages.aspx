1e8dd3b9b4476cd4dd7b1c95415e46cb66720273
Hubert Fraczkowski <UL0253543@edu.uni.lodz.pl>
2022-09-01 18:52:01 +0200

    Dodaj pliki projektów.

---
+++ GUI/Pages/LoginPages.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginPages.aspx.cs" Inherits="GUI.Pages.LoginPages" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+    <title>Logowanie - Magazyny.pl</title>
+    <link href="../Content/Style.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div class="loginbox">
+
+            <div class ="logo">
+                 <h1 class="text-with-gardient">MAGAZYNY.PL</h1>
+            </div>
+
+            <div class="register">
+                <h2>Logowanie</h2>
+            </div>
+
+            <div class ="text-box text-box-login">
+                <asp:Label ID="Label1" runat="server" Text="Login:"></asp:Label>
+                <asp:TextBox CssClass="textbox textbox-login" ID="Txb_login" runat="server"></asp:TextBox>
+            </div>
+
+            <div class ="text-box text-box-pass">
+                <asp:Label ID="Label2" runat="server" Text="Hasło:"></asp:Label>
+                <asp:TextBox CssClass="textbox textbox-pass" ID="Txb_pass" runat="server"></asp:TextBox>
+            </div>
+            
+            <div class="btn_login">
+                <asp:Button CssClass="btnlogin" ID="Btn_login" runat="server" Text="Zaloguj się!" />
+            </div>
+
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ GUI/Pages/LoginPages.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generowane automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Zmiany w tym pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowane automatycznie>
+//------------------------------------------------------------------------------
+
+namespace GUI.Pages
+{
+
+
+    public partial class LoginPages
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka Label1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Kontrolka Txb_login.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txb_login;
+
+        /// <summary>
+        /// Kontrolka Label2.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Kontrolka Txb_pass.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txb_pass;
+
+        /// <summary>
+        /// Kontrolka Btn_login.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Btn_login;
+    }
+}
