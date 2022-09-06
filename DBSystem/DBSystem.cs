b1ecf03b9350f62af6bfc91c7746b11af36a1d62
Hubert Fraczkowski <UL0253543@edu.uni.lodz.pl>
2022-09-06 04:05:34 +0200

    Dodanie DBSystem.

---
+++ DBSystem/DBSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using Npgsql;
+
+namespace DBSystem
+{
+    public class DBSystem
+    {
+        public static string connectionString = "Host=localhost;Username=postgrsql;Password=qazwsxedc;Database=Magazyny";
+        public static NpgsqlConnection npgsql = new NpgsqlConnection(connectionString);
+
+        public DataTable SelectFromDB (DataTable data, NpgsqlCommand command)
+        {
+            try
+            {
+                using (NpgsqlConnection npgsqlConnection = new NpgsqlConnection(connectionString))
+                {
+                    npgsqlConnection.Open();
+                    command.Connection = npgsqlConnection;
+                    NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(command);
+                    command.ExecuteScalar();
+                    npgsqlDataAdapter.Fill(data);
+                    return data;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
