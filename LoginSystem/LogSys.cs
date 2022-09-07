75e2e9c3e5f9e2b6c608c57854d8ec2b7e9ab2c9
Hubert Fraczkowski <UL0253543@edu.uni.lodz.pl>
2022-09-07 12:00:45 +0200

    Dodanie strony AdminMenuPage i LogSys.

---
+++ LoginSystem/LogSys.cs
@@ -0,0 +1,30 @@
+﻿using DBSystem;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    public class LogSys
+    {
+        public static void Login(string username, string password)
+        {
+            DataTable dt = new DataTable();
+            string query = "SELECT login_user, password_user FROM login_user WHERE login_user = @username AND password_user = @password;";
+            NpgsqlCommand cmd = new NpgsqlCommand(query);
+            cmd.Parameters.AddWithValue("username", username);
+            cmd.Parameters.AddWithValue("password", password);
+
+            DBSystem.DBSystem.SelectFromDB(dt, cmd);
+
+            if (dt.Rows.Count == 0)
+                return;
+
+
+        }
+    }
+}
