ce9db3b2381a3cad06f8ef1d1b39782c5ae12922
Hubert Fraczkowski <UL0253543@edu.uni.lodz.pl>
2022-09-07 13:03:15 +0200

    Stowrzenie klasy MySession.

---
+++ LoginSystem/MySession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    public class MySession
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public bool IsLogged { get; set; }
+    }
+}
